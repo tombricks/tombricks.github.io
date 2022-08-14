a3fe24e7cb2288b5b39b93985cdecd6c8c212451
tombricks <tombricks@outlook.com>
2022-08-14 16:50:36 +0100

    Conflicts

---
+++ conflicts/conflicts.json.js
@@ -0,0 +1,305 @@
+var conflictdata = {
+    "columns": [ "Europe", "Post-Soviet", "Africa and Asia", "Middle East", "Other" ],
+    "conflicts": [
+        {
+            "column": "Middle East",
+            "name": "Israeli-Palestine Conflict",
+            "side1": "ISR",
+            "side2": "PAL"
+        },
+        {
+            "column": "Middle East",
+            "name": "Golan Heights",
+            "side1": "ISR",
+            "side2": "SYR"
+        },
+        {
+            "column": "Other",
+            "name": "ISIL Conflict",
+            "side1": "ISI",
+            "side2": "GLOBE"
+        },
+        {
+            "column": "Other",
+            "name": "Al-Qaeda Conflict",
+            "side1": "ALQ",
+            "side2": "GLOBE"
+        },
+        {
+            "column": "Middle East",
+            "name": "Kurdish-Turkey Conflict",
+            "side1": "KUR",
+            "side2": "TUR"
+        },
+        {
+            "column": "Middle East",
+            "name": "Rojava Conflict",
+            "side1": "ROJ",
+            "side2": "SYR"
+        },
+        {
+            "column": "Europe",
+            "name": "Aegean Dispute",
+            "side1": "GRE",
+            "side2": "TUR"
+        },
+        {
+            "column": "Europe",
+            "name": "Cyprus Problem",
+            "side1": "CYP",
+            "side2": "NCY"
+        },
+        {
+            "column": "Post-Soviet",
+            "name": "Nagorno-Karabakh Conflict",
+            "side1": "ARM",
+            "side2": "AZR"
+        },
+        {
+            "column": "Europe",
+            "name": "Kosovo Question",
+            "side1": "SER",
+            "side2": "KOS"
+        },
+        {
+            "column": "Post-Soviet",
+            "name": "Russo-Ukrainian War",
+            "side1": "RUS",
+            "side2": "UKR"
+        },
+        {
+            "column": "Post-Soviet",
+            "name": "Donetsk Conflict",
+            "side1": "DNR",
+            "side2": "UKR"
+        },
+        {
+            "column": "Post-Soviet",
+            "name": "Luhansk Conflict",
+            "side1": "LNR",
+            "side2": "UKR"
+        },
+        {
+            "column": "Europe",
+            "name": "Catalan Independence",
+            "side1": "SPR",
+            "side2": "CAT"
+        },
+        {
+            "column": "Europe",
+            "name": "Basque Independence",
+            "side1": "SPR",
+            "side2": "BSQ"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Spanish-Morocco Question",
+            "side1": "SPR",
+            "side2": "MOR"
+        },
+        {
+            "column": "Europe",
+            "name": "Breton Independence",
+            "side1": "FRA",
+            "side2": "BRI"
+        },
+        {
+            "column": "Europe",
+            "name": "Corsican Independence",
+            "side1": "FRA",
+            "side2": "COR"
+        },
+        {
+            "column": "Europe",
+            "name": "Brexit",
+            "side1": "ENG",
+            "side2": "EUR"
+        },
+        {
+            "column": "Europe",
+            "name": "Scottish Independence",
+            "side1": "ENG",
+            "side2": "SCO"
+        },
+        {
+            "column": "Europe",
+            "name": "Welsh Independence",
+            "side1": "ENG",
+            "side2": "WAL"
+        },
+        {
+            "column": "Europe",
+            "name": "Northern England Independence",
+            "side1": "ENG",
+            "side2": "NEN"
+        },
+        {
+            "column": "Europe",
+            "name": "Northern Ireland Conflict",
+            "side1": "ENG",
+            "side2": "IRE"
+        },
+        {
+            "column": "Europe",
+            "name": "Ulster Independence",
+            "side1": "ENGIRE",
+            "side2": "ULS"
+        },
+        {
+            "column": "Other",
+            "name": "Falklands Conflict",
+            "side1": "ENG",
+            "side2": "ARG"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Western Sahrawi Conflict",
+            "side1": "MOR",
+            "side2": "SDR"
+        },
+        {
+            "column": "Middle East",
+            "name": "Iran-Saudi Arabia Conflict",
+            "side1": "IRN",
+            "side2": "SAU"
+        },
+        {
+            "column": "Middle East",
+            "name": "Yemeni Civil War",
+            "side1": "SPC",
+            "side2": "PLC"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Kashmir Conflict",
+            "side1": "IND",
+            "side2": "PAK"
+        },
+        {
+            "column": "Post-Soviet",
+            "name": "Transnistria Conflict",
+            "side1": "MOL",
+            "side2": "TRA"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Korean Conflict",
+            "side1": "ROK",
+            "side2": "PRK"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Tibetan Conflict",
+            "side1": "CHI",
+            "side2": "TIB"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Xinjiang Conflict",
+            "side1": "CHI",
+            "side2": "XIN"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Hong Kong Conflict",
+            "side1": "CHI",
+            "side2": "HKG"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Taiwanese Conflict",
+            "side1": "CHI",
+            "side2": "ROC"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Senkaku Islands Dispute",
+            "side1": "CHI",
+            "side2": "JAP"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "South China Sea Disputes",
+            "side1": "CHI",
+            "side2": "SCSD"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Kuril Islands Dispute",
+            "side1": "RUS",
+            "side2": "JAP"
+        },
+        {
+            "column": "Post-Soviet",
+            "name": "South Ossetia Conflict",
+            "side1": "GEO",
+            "side2": "SOE"
+        },
+        {
+            "column": "Post-Soviet",
+            "name": "Abkhazia Conflict",
+            "side1": "GEO",
+            "side2": "ABK"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Somaliland Conflict",
+            "side1": "SOM",
+            "side2": "SML"
+        },
+        {
+            "column": "Other",
+            "name": "Quebec Independence",
+            "side1": "CAN",
+            "side2": "QUE"
+        },
+        {
+            "column": "Other",
+            "name": "Cuban Embargo",
+            "side1": "USA",
+            "side2": "CUB"
+        },
+        {
+            "column": "Middle East",
+            "name": "Afghanistan Conflict",
+            "side1": "TLB",
+            "side2": "IRA"
+        },
+        {
+            "column": "Middle East",
+            "name": "Taliban-ISIS Conflict",
+            "side1": "TLB",
+            "side2": "ISI"
+        },
+        {
+            "column": "Post-Soviet",
+            "name": "Belarus Government",
+            "side1": "BLR",
+            "side2": "BLO"
+        },
+        {
+            "column": "Middle East",
+            "name": "Syrian Civil War",
+            "side1": "SYR",
+            "side2": "SYO"
+        },
+        {
+            "column": "Post-Soviet",
+            "name": "Russia Government",
+            "side1": "RUS",
+            "side2": "RUO"
+        },
+        {
+            "column": "Africa and Asia",
+            "name": "Tigray War",
+            "side1": "ETH",
+            "side2": "TIG"
+        },
+        {
+            "column": "Europe",
+            "name": "Bosnian Conflict",
+            "side1": "BOS",
+            "side2": "SRP"
+        }
+    ]
+}
---
+++ conflicts/main.js
@@ -0,0 +1,61 @@
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+conflictdata["columns"].forEach((column, columnI, columnA) => {
+    document.write(`<div class="column"><h1>${column}</h1><hr>`)
+    console.log(column)
+    conflictdata["conflicts"].forEach((currentValue, index, arr) => {
+        if (currentValue["column"] == column) {
+            document.write(`<h3>${currentValue["name"]}</h3>
+        <input id="conflict_${index}_side1" type="radio" name="conflict_${index}" value="${currentValue["side1"]}"><img id="conflict_${index}_side1btn" onclick="buttonClick(1, ${index})" class="flag" src="${countrydata["flags"][currentValue["side1"]]}" />
+        <input id="conflict_${index}_na" type="radio" name="conflict_${index}" value="neither">
+        <img onclick="buttonClick(2, ${index})" id="conflict_${index}_side2btn" class="flag" src="${countrydata["flags"][currentValue["side2"]]}" /><input id="conflict_${index}_side2" type="radio" name="conflict_${index}" value="${currentValue["side2"]}">
+        `)
+            document.getElementById(`conflict_${index}_na`).checked = true;
+            if (urlParams.has(`conflict${index}`)) {
+                console.log(`${index} is there: ${urlParams.get(`conflict${index}`)}`)
+                if (urlParams.get(`conflict${index}`) == "1") { document.getElementById(`conflict_${index}_side1`).checked = true; }
+                else if (urlParams.get(`conflict${index}`) == "2") { document.getElementById(`conflict_${index}_side2`).checked = true; }
+                else if (urlParams.get(`conflict${index}`) == "na") { document.getElementById(`conflict_${index}_na`).checked = true; }
+            }
+        }
+    });
+    document.write("</div>")
+});
+
+function buttonClick(side, conflict) {
+    console.log(side, conflict);
+    document.getElementById(`conflict_${conflict}_side${side}`).checked = true;
+}
+
+function share() {
+    app = window.location.href.split('?')[0] + "?"
+    conflictdata["conflicts"].forEach((currentValue, index, arr) => {
+        if (document.getElementById(`conflict_${index}_na`).checked) {
+            app += `conflict${index}=na&`
+        }
+        else if (document.getElementById(`conflict_${index}_side1`).checked) {
+            app += `conflict${index}=1&`
+        }
+        else if (document.getElementById(`conflict_${index}_side2`).checked) {
+            app += `conflict${index}=2&`
+        }
+    });
+    alert(app)
+}
+
+function clearv() {
+    console.log("clear")
+    result = confirm("Are you sure you want to clear all values?")
+    if (result) {
+        conflictdata["conflicts"].forEach((currentValue, index, arr) => {
+            document.getElementById(`conflict_${index}_na`).checked = true;
+        });
+    }
+}
+
+window.onbeforeunload = function ()
+{
+    return confirm("Are you sure you want to reload?");
+};
