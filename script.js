5ab665fe83e3069d5cb14046b07508720e5b89c0
tombricks <tombricks@outlook.com>
2024-06-10 19:22:15 +0100

    purge

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function random_choice(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
+
 function weighted_random(options, prob) {
     var out = [];
     options.forEach((option, index) => {
@@ -8,12 +9,15 @@ function weighted_random(options, prob) {
     });
     return random_choice(out);
 }
+
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function random_government() {
     var el = document.getElementById("gov_type");
+    el.innerText = capitalizeFirstLetter(random_government_2());
+    return
     var constitutional = weighted_random(["unitary ", "federal ", "confederal ", ""], [10, 7, 1, 8]);
     var party = weighted_random(["one-party ", "dominant-party ", "two-party ", "multi-party ", "non-partisan ", ""], [10, 8, 2, 4, 1, 15])
     var power = weighted_random(["parliamentary ", "presidential ", "semi ", "directorial ", "directorial parliamentary ", ""], [13, 9, 5, 1, 1, 5])
@@ -22,21 +26,17 @@ function random_government() {
     if (stype.includes("monarchy")) {
         if (power == "parliamentary ") {
             stype = stype.replace("monarchy", "constitutional monarchy");
-        }
-        else if (power == "presidential ") {
+        } else if (power == "presidential ") {
             power = "";
             stype = stype.replace("monarchy", "executive monarchy");
-        }
-        else if (power == "semi ") {
+        } else if (power == "semi ") {
             power = "parliamentary ";
             stype = stype.replace("monarchy", "semi-constitutional monarchy");
-        }
-        else if (power == "directorial parliamentary ") {
+        } else if (power == "directorial parliamentary ") {
             power = "directorial ";
             stype = stype.replace("monarchy", "constitutional monarchy");
         }
-    }
-    else {
+    } else {
         if (power == "semi ") {
             power = "semi-presidential "
         }
@@ -44,8 +44,7 @@ function random_government() {
     extra1 = ""
     if (party == "one-party " && ideology == "socialist ") {
         extra1 = weighted_random(["Marxist-Leninist ", ""], [1, 1]);
-    }
-    else if (party == "dominant-party " && ideology == "socialist ") {
+    } else if (party == "dominant-party " && ideology == "socialist ") {
         extra1 = weighted_random(["Marxist-Leninist ", ""], [1, 2]);
     }
     var total = capitalizeFirstLetter(constitutional + extra1 + party + power + ideology + stype);
@@ -60,8 +59,7 @@ function random_government_2() {
     if (state_type == "republic") {
         state_type = weighted_random(["republic", "constitutional republic"], [15, 1]);;
         executive = weighted_random(["presidential", "semi-presidential", "parliamentary"], [3, 2, 3]);
-    }
-    else {
+    } else {
         state_type = weighted_random(["monarchy", "elective monarchy", "hereditary monarchy"], [15, 2, 2]);;
         executive = weighted_random(["absolute", "parliamentary semi-constitutional", "parliamentary constitutional"], [2, 3, 10]);
     }
